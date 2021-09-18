e702a6c8fcb4e2cf3b2b6140e856458a70688aa2
superusertoor <lowercasetoor@protonmail.com>
2021-09-18 17:40:25 +0200

    Initial commit

--- src/main/java/example/FlyPerk.java
+++ src/main/java/example/FlyPerk.java
@@ -7,7 +7,7 @@ import perks.PerkPlayer;
 public class FlyPerk extends Perk {
 
     @Override
-    public void activate(PerkPlayer perkPlayer) {
+    public void test(PerkPlayer perkPlayer) {
         final Player player = perkPlayer.bukkit();
         player.setAllowFlight(true);
         player.setFlying(true);
